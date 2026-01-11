59a5f1a24369d95ef6a14d0722c282be1b8ff1f8
eichi1236 <eichi1236@gmail.com>
2026-01-11 23:23:34 +0800

    Update Type.kt

--- app/src/main/java/com/example/tolete/ui/theme/Type.kt
+++ app/src/main/java/com/example/tolete/ui/theme/Type.kt
@@ -1,1 +1,24 @@
+package com.example.tolete.ui.theme
 
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    displaySmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 36.sp,
+        lineHeight = 44.sp,
+        letterSpacing = 0.sp,
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp,
+        letterSpacing = 0.sp,
+    )
+)
